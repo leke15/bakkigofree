bd6372fb3cbe39798b925fa2f263d7a382d6194f
Daniel <lekeayomidotun@gmail.com>
2024-07-29 15:44:39 +0200

    navlink update

--- src/components/business/business.jsx
+++ src/components/business/business.jsx
@@ -1,54 +1,65 @@
-import React from 'react'
-import { HashLink as Link } from 'react-router-hash-link'
-import Drive from "../drive/Drive"
-import Earnings from "../earnings/Earnings"
-import Verification from "../verification/Verification"
-import BakkiegoPlus from '../bakkie+/BakkiegoPlus'
+import React from "react";
+import { HashLink as Link } from "react-router-hash-link";
+import Partner from "../partner/Partner";
+import Earnings from "../earnings/Earnings";
+import BakkiegoPlus from "../bakkie+/BakkiegoPlus";
+import Footer from "../footer/Footer";
 import Logo from "../../assets/logo.jpg";
-import aiImage from "../../assets/AiImage.jpg"
-import './business.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapPin } from '@fortawesome/free-solid-svg-icons/faMapPin'
-import { faRoute } from '@fortawesome/free-solid-svg-icons'
-
+import aiImage from "../../assets/AiImage.jpg";
+import "./business.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapPin } from "@fortawesome/free-solid-svg-icons/faMapPin";
+import { faRoute } from "@fortawesome/free-solid-svg-icons";
 
 const business = () => {
   return (
-    <section id='business-container'>
-      <div className='header'>
-      <img src={Logo} alt="Bakkiego" className="logo_img" />
+    <section id="business-container">
+      <div className="header">
+        <img src={Logo} alt="Bakkiego" className="logo_img" />
       </div>
 
-      <nav className='navbar'>
-      <div className="navbar-links-01">
-        <Link smooth to="#/verification" >Requirements</Link>
-        <Link smooth to="#earn" >Earning</Link>
-        <Link smooth to="/#drive" >Partner</Link>
-        <Link smooth to="/#about" >about</Link>
-      </div>
+      <nav className="navbar">
+        <div className="navbar-links-01">
+          <a href="#earn">
+            Requirements
+          </a>
+          <a href="#earn">
+            Earning
+          </a>
+          <a href="#partner">
+            Partner
+          </a>
+          <Link smooth to="/#about">
+            About
+          </Link>
+        </div>
       </nav>
-    <div id="pagebanner">
-      <div id='imgbanner'>
-        <img src={aiImage} alt="join bakkiego" id='driver_img'  />
-      </div>
-       
-      <div id="textbanner"> 
-        <FontAwesomeIcon icon={faMapPin} id='location_pin'/>
-        <h3 id='home_text'>Opportunities Everywhere</h3>
-        <h5 id='desc_text'>Make the most of your time on the road with a platform of customers looking for your services.</h5>
-      <div id="cta-01">
+      <div id="pagebanner">
+        <div id="imgbanner">
+          <img src={aiImage} alt="join bakkiego" id="driver_img" />
+        </div>
+
+        <div id="textbanner">
+          <FontAwesomeIcon icon={faMapPin} id="location_pin" />
+          <h3 id="home_text">Opportunities Everywhere</h3>
+          <h5 id="desc_text">
+            Make the most of your time on the road with a platform of customers
+            looking for your services.
+          </h5>
+          <div id="cta-01">
             <a href="#verification" download id="cta_01_btn">
               Drive
-            </a> 
+            </a>
+          </div>
+          <FontAwesomeIcon icon={faRoute} id="route_icon" />
+        </div>
       </div>
-      <FontAwesomeIcon icon={faRoute} id='route_icon'/>
-      </div>
-      </div>
-      <BakkiegoPlus/>
+      <BakkiegoPlus />
+      <Earnings />
+      <Partner />
+      <Footer />
     </section>
+  );
+};
 
-    
-  )
-}
-
-export default business
+export default business;
