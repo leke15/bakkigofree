30b4cbe4304c7ff9c9c35fec69888d8e0fbcd2d7
Daniel <lekeayomidotun@gmail.com>
2024-08-10 16:54:20 +0200

    new landing page look with the img as the background

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { HashLink as Link } from 'react-router-hash-link';
+import { HashLink as Link } from "react-router-hash-link";
 import About from "../../components/about/About";
 import Service from "../../components/services/Service";
 import DownloadApp from "../../components/downloadApp/DownloadApp";
 import Contact from "../../components/contact/Contact";
 import Footer from "../../components/footer/Footer";
 import Logo from "../../assets/logo.jpg";
-import Movers from "../../assets/movers.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import "./home.css";
@@ -26,59 +25,64 @@ const Home = () => {
         <nav className="navbar">
           {/*navigation links */}
           <div className="navbar-links navbar-links2">
-           {/* Use HashLink for smooth scroll */}
-          <Link smooth to="/#services">Services</Link>
-          <Link to="/business">Business</Link> 
-          <Link smooth to="/#about">About</Link>
-          <Link smooth to="/#home">Home</Link>
-          <Link smooth to="/#contact">Contact</Link>
-        </div>
-      </nav>
+            {/* Use HashLink for smooth scroll */}
+            <Link smooth to="/#services">
+              Services
+            </Link>
+            <Link to="/business">Business</Link>
+            <Link smooth to="/#about">
+              About
+            </Link>
+            <Link smooth to="/#home">
+              Home
+            </Link>
+            <Link smooth to="/#contact">
+              Contact
+            </Link>
+            <Link id="download-cta" href="https://play.google.com/store/apps/details?id=com.bakkiego.customer&pcampaignid=web_share">
+            Download the app
+            </Link>
+          </div>
+        </nav>
       </div>
       {/* mobile nav-bar */}
       <div className="mobile-nav-container">
         <div className="mobile-nav-buttons" onClick={toggleNav}>
-          <FontAwesomeIcon icon={isOpen ? faXmark: faBars} />
+          <FontAwesomeIcon icon={isOpen ? faXmark : faBars} />
         </div>
         {isOpen && (
           <nav className="mobile-navbar">
             <div className="mobile-navbar-links">
-            <Link smooth to="/#services">Services</Link>
-          <Link to="/business">Business</Link> 
-          <Link smooth to="/#about">About</Link>
-          <Link smooth to="/#home">Home</Link>
-          <Link smooth to="/#contact">Contact</Link>
+              <Link smooth to="/#services">
+                Services
+              </Link>
+              <Link to="/business">Business</Link>
+              <Link smooth to="/#about">
+                About
+              </Link>
+              <Link smooth to="/#home">
+                Home
+              </Link>
+              <Link smooth to="/#contact">
+                Contact
+              </Link>
             </div>
           </nav>
         )}
       </div>
-      <div className="container intro__panel">
-        <div className="right__panel">
+      {/*End*/}
+      <div className="landing-container">
+        <div>
           <h2 className="motto">
-            Connecting you to your nearest delivery service
+            Innovative <br/> Technologies <br/> Involving the <br/>Community
           </h2>
-          <p className="motto_text">
-            Bakkiego is a delivery and haulage app designed to provide
-            individuals and businesses in South Africa with a flexible,
-            convenient, and affordable means of transporting their goods.
-          </p>
-
-          <div className="cta">
-            <a href="#download" download className="btn">
-              Get Bakkie Now
-            </a>
-            <Link to="/business" className="btn btn-primary">
-              Earn Money
-            </Link>
-          </div>
         </div>
-        <img src={Movers} alt="Movers" className="panel__img" />
       </div>
       <About />
       <Service />
       <DownloadApp />
       <Contact />
-      <Footer/>
+      <Footer />
     </section>
   );
 };
