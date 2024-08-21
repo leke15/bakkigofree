4f6d3831fd454841d52eedaed2fc022bfda05ee3
Daniel <lekeayomidotun@gmail.com>
2024-08-21 20:08:30 +0200

    Created the services Route Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./components/home/Home";
-import Business from "./components/business/business";
+import Service from "./components/services/Service";
 import About from './components/about/About'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/business" element={<Business />} />
+        <Route path="/Service" element={<Service />} />
         <Route path="/" element={<Home/>} />
       </Routes>
     </BrowserRouter>
