1f4ac96b610c8dc75ec2b305436f965fef10c163
Daniel <lekeayomidotun@gmail.com>
2024-08-21 20:11:30 +0200

    changed the button text and added the link for request to go to the service component

--- src/components/services/ServiceLanding.jsx
+++ src/components/services/ServiceLanding.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./servland.css"
+import { HashLink as Link } from "react-router-hash-link";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPeopleGroup, faTruckFast } from '@fortawesome/free-solid-svg-icons'
 
@@ -10,7 +11,7 @@ function ServiceLanding() {
             <div id="title"><h3>Our Services</h3></div>
             <div className='service-container service1'>
             <FontAwesomeIcon icon={faTruckFast} className='service-icon'/> <br/>
-            <button className='btn service-btn'>order bakkie</button>
+            <button className='btn service-btn' > <Link smooth to="/Service"> Request a bakkie</Link></button>
             </div>
             <div className='service-container service2'>
             <FontAwesomeIcon icon={faPeopleGroup} className='service-icon'/> <br/>
