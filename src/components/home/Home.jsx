e90cec6f310b136f041740b5f4f571122cdb0ec4
Daniel <lekeayomidotun@gmail.com>
2024-08-26 17:52:38 +0200

    seperated the Navbar to it's own component to be used globally

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,92 +1,22 @@
-import React, { useState } from "react";
-import { HashLink as Link } from "react-router-hash-link";
-import About from "../../components/about/About";
 import Features from "../services/Features";
 import DownloadApp from "../../components/downloadApp/DownloadApp";
 import Contact from "../../components/contact/Contact";
 import Footer from "../../components/footer/Footer";
 import ServiceLanding from "../services/ServiceLanding";
-import Logo from "../../assets/logo.jpg";
+import Navbar from "./Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faAnglesDown,
-  faBars,
-  faXmark,
-} from "@fortawesome/free-solid-svg-icons";
+import { faAnglesDown } from "@fortawesome/free-solid-svg-icons";
 import "./home.css";
-import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleNav = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <section className="container">
-      <div className="header">
-        <img src={Logo} alt="Bakkiego" className="logo_img" />
-
-        <nav className="navbar">
-          {/*navigation links */}
-          <div className="navbar-links navbar-links2">
-            {/* Use HashLink for smooth scroll */}
-            <Link smooth to="/#home">
-              Home
-            </Link>
-            <Link smooth to="/#about">
-              About
-            </Link>
-            <Link smooth to="/Service">
-              Services
-            </Link>
-            <Link smooth to="/#contact">
-              Contact
-            </Link>
-            <Link id="whatsapp-cta" smooth to="https://wa.me/27696680474">
-              <FontAwesomeIcon icon={faWhatsapp} id="whatsapp-icon" />
-              +27 696680474
-            </Link>
-            <Link
-              id="download-cta"
-              smooth to="https://play.google.com/store/apps/details?id=com.bakkiego.customer&pcampaignid=web_share"
-            >
-              Download the app
-            </Link>
-          </div>
-        </nav>
-      </div>
-      {/* mobile nav-bar */}
-      <div className="mobile-nav-container">
-        <div className="mobile-nav-buttons" onClick={toggleNav}>
-          <FontAwesomeIcon icon={isOpen ? faXmark : faBars} />
-        </div>
-        {isOpen && (
-          <nav className="mobile-navbar">
-            <div className="mobile-navbar-links">
-              <Link smooth to="/#services">
-                Services
-              </Link>
-              <Link to="/business">Business</Link>
-              <Link smooth to="/#about">
-                About
-              </Link>
-              <Link smooth to="/#home">
-                Home
-              </Link>
-              <Link smooth to="/#contact">
-                Contact
-              </Link>
-            </div>
-          </nav>
-        )}
-      </div>
-      {/*End*/}
+      <Navbar />
       <div className="landing-container">
         <div>
           <h2 className="motto">
-            Move Your  <br />Goods <br /> Anywhere  <br />
+            Move Your <br />
+            Goods <br /> Anywhere <br />
             AnyTime
           </h2>
           <FontAwesomeIcon icon={faAnglesDown} id="scroll-down" />
---
+++ src/components/home/Navbar.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { faWhatsapp} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { HashLink as Link } from "react-router-hash-link";
+import About from "../../components/about/About";
+import Logo from "../../assets/logo.jpg";
+import "./navbar.css";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div className="header">
+      <img src={Logo} alt="Bakkiego" className="logo_img" />
+
+      <nav className="navbar">
+        {/*navigation links */}
+        <div className="navbar-links navbar-links2">
+          {/* Use HashLink for smooth scroll */}
+          <Link smooth to="/#home">
+            Home
+          </Link>
+          <Link smooth to="/#about">
+            About
+          </Link>
+          <Link smooth to="/Service">
+            Services
+          </Link>
+          <Link smooth to="/#contact">
+            Contact
+          </Link>
+          <Link id="whatsapp-cta" smooth to="https://wa.me/27696680474">
+            <FontAwesomeIcon icon={faWhatsapp} id="whatsapp-icon" />
+            +27 696680474
+          </Link>
+          <Link
+            id="download-cta"
+            smooth
+            to="https://play.google.com/store/apps/details?id=com.bakkiego.customer&pcampaignid=web_share"
+          >
+            Download the app
+          </Link>
+        </div>
+      </nav>
+      {/* mobile nav-bar */}
+      <div className="mobile-nav-container">
+        <div className="mobile-nav-buttons" onClick={toggleNav}>
+          <FontAwesomeIcon icon={isOpen ? faXmark : faBars} />
+        </div>
+        {isOpen && (
+          <nav className="mobile-navbar">
+            <div className="mobile-navbar-links">
+              <Link smooth to="/#services">
+                Services
+              </Link>
+              <Link to="/business">Business</Link>
+              <Link smooth to="/#about">
+                About
+              </Link>
+              <Link smooth to="/#home">
+                Home
+              </Link>
+              <Link smooth to="/#contact">
+                Contact
+              </Link>
+            </div>
+          </nav>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
