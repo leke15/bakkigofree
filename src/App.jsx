3b9f74c616604f5a19ac1e1a8cef9c192a92c06c
Daniel <lekeayomidotun@gmail.com>
2024-09-02 19:31:29 +0200

    created the drivers link

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./components/home/Home";
 import Service from "./components/services/Service";
-import About from './components/about/About'
-
+import Business from "./components/business/business"
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/Service" element={<Service />} />
+        <Route path="/Business" element={<Business />} />
         <Route path="/" element={<Home/>} />
       </Routes>
     </BrowserRouter>
