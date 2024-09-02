55de1cdd6b781ce1a332fab4c90fd9be162d1433
Daniel <lekeayomidotun@gmail.com>
2024-09-02 20:03:38 +0200

    changed the icons and text sizes as well as their positions

--- src/components/business/business.jsx
+++ src/components/business/business.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { HashLink as Link } from "react-router-hash-link";
-import Partner from "../partner/Partner";
-import Earnings from "../earnings/Earnings";
-import BakkiegoPlus from "../bakkie+/BakkiegoPlus";
+import Navbar from "../home/Navbar";
+import BakkiegoPlus from "../bakkie+/BakkiegoPlus"
+import Earnings from "../earnings/Earnings"
+import Partner from "../partner/Partner"
 import Footer from "../footer/Footer";
-import Logo from "../../assets/logo.jpg";
 import aiImage from "../../assets/AiImage.jpg";
 import "./business.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -14,40 +13,21 @@ import { faRoute } from "@fortawesome/free-solid-svg-icons";
 const business = () => {
   return (
     <section id="business-container">
-      <div className="header">
-        <img src={Logo} alt="Bakkiego" className="logo_img" />
-      </div>
-
-      <nav className="navbar">
-        <div className="navbar-links-01">
-          <a href="#earn">
-            Requirements
-          </a>
-          <a href="#earn">
-            Earning
-          </a>
-          <a href="#partner">
-            Partner
-          </a>
-          <Link smooth to="/#about">
-            About
-          </Link>
-        </div>
-      </nav>
-      <div id="pagebanner">
-        <div id="imgbanner">
+      <Navbar /> 
+      <div className="pagebanner">
+        <div className="imgbanner">
           <img src={aiImage} alt="join bakkiego" id="driver_img" />
         </div>
 
-        <div id="textbanner">
+        <div className="textbanner">
           <FontAwesomeIcon icon={faMapPin} id="location_pin" />
-          <h3 id="home_text">Opportunities Everywhere</h3>
-          <h5 id="desc_text">
+          <h3 className="home_text">Opportunities Everywhere</h3>
+          <p className="desc_text">
             Make the most of your time on the road with a platform of customers
             looking for your services.
-          </h5>
-          <div id="cta-01">
-            <a href="#verification" download id="cta_01_btn">
+          </p>
+          <div className="cta-01">
+            <a href="#verification" className="cta_01_btn">
               Drive
             </a>
           </div>
