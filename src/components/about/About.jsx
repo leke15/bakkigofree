bfd6c1f6b00c40203b80a9c72207303f55ec1a6c
Daniel <lekeayomidotun@gmail.com>
2024-09-02 21:20:59 +0200

    New about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,77 +1,41 @@
 import React from "react";
-import bakkiedash from "../../assets/bakkiedash.jpeg";
-import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Navbar from "../home/Navbar";
+import Footer from "../footer/Footer";
+import ceo from "../../assets/ceo.jpg";
 import "./about.css";
 const About = () => {
   return (
     <section id="about">
-      {/* Right side of the about page: Image and a cta button */}
-      <div className="right-panel">
-        <img src={bakkiedash} alt="Bakkie Dashboard" className="car-img" />
-        <div id="earn">
-          <a href="">Earn Money</a>
+      <Navbar />
+      <h1 className="about-heading"> About </h1>
+      <div className="about-container">
+        <h2 className="desc-heading">What We Do</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+          quibusdam perspiciatis deleniti, id fuga impedit vero nulla excepturi
+          enim a magnam nisi asperiores necessitatibus velit ab. Molestiae
+          veritatis corrupti incidunt.
+        </p>
+      </div>
+      <h3 className="team-heading">Our Team</h3>
+      <div className="team-container">
+      <div className="team-member">
+          <img src={ceo} alt="David Ayomidotun" className="team-img" />
+          <div className="team-info">
+            <p>
+              David Ayomidotun <br /> C.E.O
+            </p>
+          </div>
+          
+          <img src={ceo} alt="David Ayomidotun" className="team-img" />
+          <div className="team-info">
+            <p>
+              David Ayomidotun <br /> C.E.O
+            </p>
+          </div>
         </div>
       </div>
-
-      {/* Left side of the about page: Introduction to bakkiego and our services */}
-      <div className="left-panel">
-        <h2 className="about-header">
-          The Best On-demand Booking Platform to Move your Goods anywhere at
-          Anytime
-        </h2>
-
-        <h4 className="about-text">
-          Online freight booking systems in South Africa often struggle with
-          high fixed prices, making them unaffordable for customers. Our mission
-          is to provide reliable and comfortable services to all movers of
-          goods, allowing them to name their own price.
-        </h4>
-
-        {/* Outline Section 1 */}
-        <div className="outline">
-          <div className="outline-item">
-            <div className="tick-icon">
-              <FontAwesomeIcon icon={faCircleCheck} />
-            </div>
-            <div className="outline-text">
-              <h5>
-                Lifters Option : We know that sometimes it requires more than
-                yourself to lift the goods into your truck so we have the option
-                of lifters included in our App.
-              </h5>
-            </div>
-          </div>
-
-          {/* Outline Section 2 */}
-          <div className="outline-item">
-            <div className="tick-icon">
-              <FontAwesomeIcon icon={faCircleCheck} />
-            </div>
-            <div className="outline-text">
-              <h5>
-                Picture Demand: Sometimes Distance and verbal conversations do
-                not explain what you need to pick up, so we also have picture
-                demand before you bargain
-              </h5>
-            </div>
-          </div>
-          {/* Outline Section 3 */}
-          <div className="outline-item">
-            <div className="tick-icon">
-              <FontAwesomeIcon icon={faCircleCheck} />
-            </div>
-            <div className="outline-text">
-              <h5>
-                Open bargain : Your price is determined by a fair bargaining
-                system on our App, customer offers a price you either accept or
-                make a counter offer
-              </h5>
-            </div>
-          </div>
-        </div>
-        {/* End of section outline */}
-      </div>
+      <Footer />
     </section>
   );
 };
