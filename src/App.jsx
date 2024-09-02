35f5788203be66b0b2b3cbf28653b0f195f11d68
Daniel <lekeayomidotun@gmail.com>
2024-09-02 21:20:39 +0200

    Created about route

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./components/home/Home";
 import Service from "./components/services/Service";
-import Business from "./components/business/business"
+import Business from "./components/business/business";
+import About from "./components/about/About";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/Service" element={<Service />} />
         <Route path="/Business" element={<Business />} />
+        <Route path="/About" element={<About />} />
         <Route path="/" element={<Home/>} />
       </Routes>
     </BrowserRouter>
