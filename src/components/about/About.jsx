c07b28a404432cc00c15ad6b96d083a27dbaba64
Daniel <lekeayomidotun@gmail.com>
2024-09-11 19:10:43 +0200

    About us message

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,35 +2,44 @@ import React from "react";
 import Navbar from "../home/Navbar";
 import Footer from "../footer/Footer";
 import ceo from "../../assets/ceo.jpg";
+import ctm from "../../assets/ctm.jpg";
 import "./about.css";
 const About = () => {
   return (
     <section id="about">
       <Navbar />
-      <h1 className="about-heading"> About </h1>
+      <h1 className="about-heading"> Let me introduce you to an <b>IDEA...</b></h1>
       <div className="about-container">
-        <h2 className="desc-heading">What We Do</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-          quibusdam perspiciatis deleniti, id fuga impedit vero nulla excepturi
-          enim a magnam nisi asperiores necessitatibus velit ab. Molestiae
-          veritatis corrupti incidunt.
-        </p>
+        <div className="about-desc">
+          <p>
+            Moving goods, personal items, or merchandise is always a hassle and
+            the current solutions provided are not helping but are becoming a
+            problem as well. Bakkiego comes in between bridging the current
+            inefficient solutions and the problem of hassle movement with the
+            number one solution South Africa acknowledges as the "informal
+            alternative",<b>BAKKIES</b>, we all know they are flexible,
+            tenacious, and always ready to help. Not only are we involving the
+            local working community of these amazing movers but we are giving
+            them a platform to extend their services by using their trucks which
+            we all know and trust connecting formal or informal uses for a price
+            both parties can agree on by themselves.
+          </p>
+        </div>
       </div>
       <h3 className="team-heading">Our Team</h3>
       <div className="team-container">
-      <div className="team-member">
+        <div className="team-member">
           <img src={ceo} alt="David Ayomidotun" className="team-img" />
           <div className="team-info">
             <p>
               David Ayomidotun <br /> C.E.O
             </p>
           </div>
-          
-          <img src={ceo} alt="David Ayomidotun" className="team-img" />
+
+          <img src={ctm} alt="Nolo Letlaka" className="team-img2" />
           <div className="team-info">
             <p>
-              David Ayomidotun <br /> C.E.O
+              Nolo Letlaka <br /> C.M.O
             </p>
           </div>
         </div>
